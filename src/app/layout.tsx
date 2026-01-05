d7e8ca70a143a3c2ec39a2acecd4790cc54610a1
Tahmid Alam <mdtahmidalam122@gmail.com>
2026-01-05 11:50:02 +0600

    Footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} antialiased`}>{children}</body>
+      <body className={`${roboto.variable} antialiased`}>
+        <div className="min-h-screen">{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="container flex items-center justify-center py-5 border-t border-t-slate-800">
+      <p className="font-medium text-gray-400">
+        &copy; All rights reserved | 2026 Wikii Movies
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
