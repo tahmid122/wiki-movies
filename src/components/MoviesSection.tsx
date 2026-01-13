103dd5bd8017ef6c0bb5f5707a65c647a8afcaaa
Tahmid Alam <mdtahmidalam122@gmail.com>
2026-01-13 13:08:41 +0600

    Movies shows container grid gap fiexed

--- src/components/MoviesSection.tsx
+++ src/components/MoviesSection.tsx
@@ -12,7 +12,7 @@ const MoviesSection = async ({ endpoint, page, other }: Props) => {
     notFound();
   }
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-2 lg:gap-4">
       {/* card */}
       {movies.results.slice(0, 18).map((movie) => (
         <MoviesCard key={movie?.id} movie={movie} />
