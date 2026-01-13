118a6edb89c47077630b9278d500928dda186f3f
Tahmid Alam <mdtahmidalam122@gmail.com>
2026-01-13 10:50:02 +0600

    Navbar for mobile done

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,44 +1,37 @@
-import { TMDB_BASE_URL } from "@/utilities/baseUrls";
-import Image from "next/image";
+"use client";
+
 import Link from "next/link";
-import { FaAngleDown } from "react-icons/fa";
-import { FaBars, FaMagnifyingGlass, FaXmark } from "react-icons/fa6";
-import MobileNav from "./MobileNav";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { FaAngleDown, FaXmark } from "react-icons/fa6";
 interface Genre {
   id: number;
   name: string;
 }
-const NavBar = async () => {
-  const res = await fetch(
-    `${TMDB_BASE_URL}/genre/movie/list?api_key=${process.env.TMDB_API_KEY}`,
-    { next: { revalidate: 60 } }
-  );
-  if (!res.ok) {
-    throw new Error(`HTTP error! status: ${res.status}`);
-  }
-  const genres: { genres: Genre[] } = await res.json();
+interface Props {
+  genres: {
+    genres: Genre[];
+  };
+}
+const MobileNav = ({ genres }: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <header className="fixed left-0 top-0 py-2 lg:py-3 w-full z-40 bg-black border-b border-b-slate-700">
-      <nav className="container flex items-center justify-between">
-        {/* logo */}
-
-        <Link href={"/"}>
-          <div className="text-xl xl:text-xl uppercase font-bold text-white text-shadow-2xs flex items-center">
-            Wikii
-            <div className="w-12 h-12 relative rounded-full overflow-hidden">
-              <Image
-                src={"/movielogo.jpg"}
-                fill
-                alt="Wikii movies"
-                className="object-cover"
-              />
-            </div>{" "}
-            Movies
-          </div>
-        </Link>
-
-        {/* Nav links */}
-        <ul className="md:flex hidden items-center gap-3 text-base">
+    <div className="lg:hidden">
+      <span className="cursor-pointer" onClick={(): void => setIsOpen(true)}>
+        <FaBars className="text-[22px]" />
+      </span>
+      {/* links */}
+      <div
+        className={`w-full bg-[#000000b4] h-screen ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } absolute left-0 top-0 z-50 transition-all duration-300`}
+      >
+        <ul className="lg:hidden flex flex-col h-full overflow-y-auto no-scrollbar2 items-start gap-3 text-base bg-black w-[70%] p-5">
+          <li className="flex items-center justify-end w-full sticky top-0 right-0 z-999">
+            <span onClick={(): void => setIsOpen(false)}>
+              <FaXmark className="text-[24px] cursor-pointer" />
+            </span>
+          </li>
           <li>
             <Link
               className="hover:text-red-500 transition-all duration-300"
@@ -47,17 +40,17 @@ const NavBar = async () => {
               Home
             </Link>
           </li>
-          <li>
-            <div className="relative group">
-              <span className="hover:text-red-500 transition-all duration-300 flex items-center justify-center gap-1 cursor-pointer">
+          <li className="w-full">
+            <div className="relative group w-full">
+              <span className="hover:text-red-500 transition-all duration-300 flex items-center justify-start gap-1 cursor-pointer">
                 Genres <FaAngleDown />
               </span>
               {/* sub genres */}
-              <ul className="absolute hidden group-hover:flex left-0 top-6 w-60 bg-black border border-slate-700 p-3  flex-col gap-3 rounded max-h-[60vh] overflow-y-auto no-scrollbar2">
+              <ul className="hidden group-hover:flex bg-black flex-col gap-3 rounded w-full ml-5 pt-5">
                 {genres.genres &&
                   genres.genres?.length > 0 &&
                   genres.genres.map((genre) => (
-                    <li key={genre?.id}>
+                    <li key={genre?.id} onClick={(): void => setIsOpen(false)}>
                       <Link
                         className="hover:text-red-500 transition-all duration-300 hover:ml-2 relative w-full block"
                         href={`/movies/genres/${genre.name.toLocaleLowerCase()}/${
@@ -71,14 +64,14 @@ const NavBar = async () => {
               </ul>
             </div>
           </li>
-          <li>
+          <li className="w-full">
             <div className="relative group">
-              <span className="hover:text-red-500 transition-all duration-300 flex items-center justify-center gap-1 cursor-pointer">
+              <span className="hover:text-red-500 transition-all duration-300 flex items-center justify-start gap-1 cursor-pointer">
                 Categories <FaAngleDown />
               </span>
               {/* sub categories */}
-              <ul className="absolute hidden group-hover:flex left-0 top-6 w-60 bg-black border border-slate-700 p-3  flex-col gap-3 rounded">
-                <li>
+              <ul className="hidden group-hover:flex left-0 top-6 w-full bg-black 3  flex-col gap-3 rounded ml-5 pt-5">
+                <li onClick={(): void => setIsOpen(false)}>
                   <Link
                     className="hover:text-red-500 transition-all duration-300 hover:ml-2 relative w-full block"
                     href={"/movies/trending/1"}
@@ -86,7 +79,7 @@ const NavBar = async () => {
                     Trending
                   </Link>
                 </li>
-                <li>
+                <li onClick={(): void => setIsOpen(false)}>
                   <Link
                     className="hover:text-red-500 transition-all duration-300 hover:ml-2 relative w-full block"
                     href={"/movies/popular/1"}
@@ -94,7 +87,7 @@ const NavBar = async () => {
                     Popular
                   </Link>
                 </li>
-                <li>
+                <li onClick={(): void => setIsOpen(false)}>
                   <Link
                     className="hover:text-red-500 transition-all duration-300 hover:ml-2 relative w-full block"
                     href={"/movies/top_rated/1"}
@@ -102,7 +95,7 @@ const NavBar = async () => {
                     Top Rated
                   </Link>
                 </li>
-                <li>
+                <li onClick={(): void => setIsOpen(false)}>
                   <Link
                     className="hover:text-red-500 transition-all duration-300 hover:ml-2 relative w-full block"
                     href={"/movies/upcoming/1"}
@@ -110,7 +103,7 @@ const NavBar = async () => {
                     Upcoming
                   </Link>
                 </li>
-                <li>
+                <li onClick={(): void => setIsOpen(false)}>
                   <Link
                     className="hover:text-red-500 transition-all duration-300 hover:ml-2 relative w-full block"
                     href={"/movies/now_playing/1"}
@@ -125,21 +118,9 @@ const NavBar = async () => {
             <Link href={"/search"}>Search</Link>
           </li>
         </ul>
-        {/* Buttons */}
-        <div className="flex items-center gap-2">
-          <Link
-            href={"/search"}
-            className="text-xl w-10 h-10 rounded flex items-center justify-center hover:bg-white/10"
-          >
-            <FaMagnifyingGlass className="text-xl" />
-          </Link>
-          {/* Mobile Nav links */}
-          <MobileNav genres={genres} />
-        </div>
-      </nav>
-      {/* Mobile nav links */}
-    </header>
+      </div>
+    </div>
   );
 };
 
-export default NavBar;
+export default MobileNav;
